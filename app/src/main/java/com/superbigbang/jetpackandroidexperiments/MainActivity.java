9914cd21b5d835f9baf9e0468330fdb1816bcd73
SuperBigBang <superbigbang@yandex.ru>
2019-04-11 15:32:18 +0900

    feat: add Timber library implementation feat: add ViewModel observe to View

--- app/src/main/java/com/superbigbang/jetpackandroidexperiments/MainActivity.java
+++ app/src/main/java/com/superbigbang/jetpackandroidexperiments/MainActivity.java
@@ -1,14 +1,43 @@
 package com.superbigbang.jetpackandroidexperiments;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.superbigbang.jetpackandroidexperiments.model.ListIssuesViewModel;
+
+import java.util.List;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProviders;
+import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListIssuesViewModel mViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mViewModel = ViewModelProviders.of(this).get(ListIssuesViewModel.class);
+        //setupView();
+        // Handle changes emitted by LiveData
+        mViewModel.getApiResponse().observe(this, apiResponse -> {
+            if (apiResponse.getError() != null) {
+                handleError(apiResponse.getError());
+            } else {
+                handleResponse(apiResponse.getIssues());
+            }
+        });
+    }
+
+    void handleError(Throwable error) {
+        Timber.e(error);
+        Toast.makeText(this, "Get ERROR, show info in Logcat", Toast.LENGTH_LONG).show();
+    }
+
+    void handleResponse(List issues) {
+        //operate
+        Toast.makeText(this, "Succeful get list of issueses", Toast.LENGTH_LONG).show();
     }
 }

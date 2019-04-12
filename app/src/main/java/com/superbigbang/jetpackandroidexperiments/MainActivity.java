e210caa19c7673b402c6dc79798c19dfb2e77c8b
SuperBigBang <superbigbang@yandex.ru>
2019-04-12 15:16:54 +0900

    feat: bind main activity to layout with butterknife feat: realise issue getting info on app feat: add Timber, now issue info send to console fix: add Internet permission. Crush fix

--- app/src/main/java/com/superbigbang/jetpackandroidexperiments/MainActivity.java
+++ app/src/main/java/com/superbigbang/jetpackandroidexperiments/MainActivity.java
@@ -1,24 +1,42 @@
 package com.superbigbang.jetpackandroidexperiments;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.superbigbang.jetpackandroidexperiments.model.ListIssuesViewModel;
+import com.superbigbang.jetpackandroidexperiments.model.response.Issue;
 
 import java.util.List;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.RecyclerView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.ownerName)
+    EditText ownerName;
+    @BindView(R.id.repositoryName)
+    EditText repositoryName;
+    @BindView(R.id.button)
+    Button button;
+    @BindView(R.id.listOfIssueses)
+    RecyclerView listOfIssueses;
     private ListIssuesViewModel mViewModel;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Timber.plant(new Timber.DebugTree()); //next time move it in Application class
+        ButterKnife.bind(this);
         mViewModel = ViewModelProviders.of(this).get(ListIssuesViewModel.class);
         //setupView();
         // Handle changes emitted by LiveData
@@ -38,6 +56,22 @@ public class MainActivity extends AppCompatActivity {
 
     void handleResponse(List issues) {
         //operate
-        Toast.makeText(this, "Succeful get list of issueses", Toast.LENGTH_LONG).show();
+        if (issues != null && issues.get(0) != null) {
+            Issue issue = (Issue) issues.get(0);
+            Timber.e(issue.getTitle());
+            Timber.e(issue.getId().toString());
+            Timber.e(issue.getUser().getLogin());
+            Toast.makeText(this, "Succeful get list of issueses", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "No data found for this query", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @OnClick(R.id.button)
+    public void onButtonClicked() {
+        if (ownerName.getText().length() == 0 || repositoryName.getText().length() == 0) {
+            Toast.makeText(this, "Please enter Owner name and Repository", Toast.LENGTH_LONG).show();
+        } else
+            mViewModel.loadIssues(ownerName.getText().toString(), repositoryName.getText().toString());
     }
 }

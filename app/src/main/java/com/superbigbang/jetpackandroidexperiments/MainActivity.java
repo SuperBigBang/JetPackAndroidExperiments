329cca6af26fa56066148cc7d70c031a45a0b31e
SuperBigBang <superbigbang@yandex.ru>
2019-04-19 14:20:56 +0900

    refactor: move code, rename infiniteL.S. to > ResponseIssuesLoadingSection fix: adaptation deprecated method getColor

--- app/src/main/java/com/superbigbang/jetpackandroidexperiments/MainActivity.java
+++ app/src/main/java/com/superbigbang/jetpackandroidexperiments/MainActivity.java
@@ -1,7 +1,7 @@
 package com.superbigbang.jetpackandroidexperiments;
 
+import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,8 +16,6 @@ import com.superbigbang.jetpackandroidexperiments.model.HeaderItem;
 import com.superbigbang.jetpackandroidexperiments.model.response.Issue;
 import com.superbigbang.jetpackandroidexperiments.viewModels.MainActivityViewModel;
 import com.xwray.groupie.GroupAdapter;
-import com.xwray.groupie.Item;
-import com.xwray.groupie.OnItemLongClickListener;
 import com.xwray.groupie.Section;
 
 import java.util.List;
@@ -30,44 +28,23 @@ public class MainActivity extends AppCompatActivity {
 
     public static final int LAYOUT = R.layout.activity_main;
 
-    Section infiniteLoadingSection;
+    Section fromResponseIssuesLoadingSection;
     private MainActivityViewModel mViewModel;
     private ActivityMainBinding binding;
     private GroupAdapter groupAdapter;
     private GridLayoutManager layoutManager;
 
-    private CardItem.OnCardItemChildClickListener onCardItemChildClickListener = new CardItem.OnCardItemChildClickListener() {
-        @Override
-        public void OnChildClick(CardItem item, View view) {
-            switch (view.getId()) {
-                case R.id.author_avatar:
-                    Toast.makeText(MainActivity.this, item.getAuthor_avatar(), Toast.LENGTH_SHORT).show();
-                    break;
-                case R.id.creatorName:
-                    Toast.makeText(MainActivity.this, item.getCreatorName(), Toast.LENGTH_SHORT).show();
-                    break;
-                case R.id.titleOfIssue:
-                    Toast.makeText(MainActivity.this, "Click on :" + item.getTitleOfIssue(), Toast.LENGTH_LONG).show();
-                    break;
-            }
-        }
-    };
-
-
-    private OnItemLongClickListener onItemLongClickListener = new OnItemLongClickListener() {
-        @Override
-        public boolean onItemLongClick(Item item, View view) {
-      /*      if (item instanceof CardItem) {
-                CardItem cardItem = (CardItem) item;
-                if (!TextUtils.isEmpty(cardItem.getText())) {
-                    Toast.makeText(MainActivity.this, "Long clicked: " + cardItem.getText(), Toast.LENGTH_SHORT).show();
-                    return true;
-                }
-            }
-            return false;
-        }
-    };*/
-            return false;
+    private CardItem.OnCardItemChildClickListener onCardItemChildClickListener = (item, view) -> {
+        switch (view.getId()) {
+            case R.id.author_avatar:
+                Toast.makeText(MainActivity.this, item.getAuthor_avatar(), Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.creatorName:
+                Toast.makeText(MainActivity.this, item.getCreatorName(), Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.titleOfIssue:
+                Toast.makeText(MainActivity.this, "Click on :" + item.getTitleOfIssue(), Toast.LENGTH_LONG).show();
+                break;
         }
     };
 
@@ -107,17 +84,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void populateAdapter(List issues) {
-        infiniteLoadingSection = new Section(new HeaderItem(R.string.issues_of_this_repository));
+        fromResponseIssuesLoadingSection = new Section(new HeaderItem(R.string.issues_of_this_repository));
         for (int i = 0; i < issues.size(); i++) {
-            CardItem cardItem = new CardItem(getResources().getColor(R.color.blue_200),
-                    ((Issue) issues.get(i)).getTitle(),
-                    ((Issue) issues.get(i)).getUser().getLogin(),
-                    ((Issue) issues.get(i)).getUser().getAvatarUrl(),
-                    onCardItemChildClickListener);
+            CardItem cardItem = null;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                cardItem = new CardItem(getResources().getColor(R.color.blue_200, getTheme()),
+                        ((Issue) issues.get(i)).getTitle(),
+                        ((Issue) issues.get(i)).getUser().getLogin(),
+                        ((Issue) issues.get(i)).getUser().getAvatarUrl(),
+                        onCardItemChildClickListener);
+            } else {
+                //Deprecated for versions API =>23
+                cardItem = new CardItem(getResources().getColor(R.color.blue_200),
+                        ((Issue) issues.get(i)).getTitle(),
+                        ((Issue) issues.get(i)).getUser().getLogin(),
+                        ((Issue) issues.get(i)).getUser().getAvatarUrl(),
+                        onCardItemChildClickListener);
+            }
+
             // cardItem.setOnItemClickListener();
-            infiniteLoadingSection.add(cardItem);
+            fromResponseIssuesLoadingSection.add(cardItem);
         }
-        groupAdapter.add(infiniteLoadingSection);
+        groupAdapter.add(fromResponseIssuesLoadingSection);
     }
 
     void handleResponse(List issues) {
@@ -147,5 +135,22 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+    };*/
+
+    /* private OnItemLongClickListener onItemLongClickListener = new OnItemLongClickListener() {
+        @Override
+        public boolean onItemLongClick(Item item, View view) {
+            if (item instanceof CardItem) {
+                CardItem cardItem = (CardItem) item;
+                if (!TextUtils.isEmpty(cardItem.getText())) {
+                    Toast.makeText(MainActivity.this, "Long clicked: " + cardItem.getText(), Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+            }
+            return false;
+        }
+    };
+            return false;
+        }
     };*/
 }
